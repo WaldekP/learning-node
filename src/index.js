911b2d58c81a5eaa8715abc87056af8edb131671
Waldek <waldek.pieniak@gmail.com>
2019-05-14 23:44:50 +0200

    Express basic rest api implemented

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import 'dotenv/config';
+import bodyParser from 'body-parser';
+import models from './models/index'
+import routes from './routes'
 
 import cors from 'cors';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.get('/data', (req, res) => res.send({message: 'Cos'}));
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next()
+})
+
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+
+
 app.listen(process.env.PORT, () => console.log('Listening on port ', 3000));
